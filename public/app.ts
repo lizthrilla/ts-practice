1b78dddbc3abea3a214fa5e26fc5a9f0f53c14e2
Liz Tiller <elizabeth.tiller@gmail.com>
2020-02-07 13:18:59 -0500

    TS

---
+++ public/app.ts
@@ -0,0 +1,6 @@
+const startGame = () => {
+    const messagesElement = document.getElementById('messages')
+    messagesElement.innerText = 'Welcome to MultiMath!  Starting new game...'
+}
+
+document.getElementById('startGame').addEventListener('click', startGame)
