93418e46d404070d14e43b05f060c757a8d68526
Liz Tiller <elizabeth.tiller@gmail.com>
2020-02-07 16:46:12 -0500

    tsconfig

--- js/app.js
+++ js/app.js
@@ -1,5 +1,7 @@
-var startGame = function () {
-    var messagesElement = document.getElementById('messages');
+"use strict";
+const startGame = () => {
+    const messagesElement = document.getElementById('messages');
     messagesElement.innerText = 'Welcome to MultiMath!  Starting new game...';
 };
 document.getElementById('startGame').addEventListener('click', startGame);
+//# sourceMappingURL=app.js.map
