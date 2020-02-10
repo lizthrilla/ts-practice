5880c9912199bbeeb26f03b9221da715c2682a8e
Liz Tiller <elizabeth.tiller@gmail.com>
2020-02-10 13:57:39 -0500

    more examples

--- app/app.ts
+++ app/app.ts
@@ -1,15 +1,52 @@
 const startGame = () => {
+  let playerName: string | undefined = getInputValue("playerName");
+  logPlayer(playerName);
 
-    const playerName: string = 'Liz'
-    logPlayer(playerName)
+  // let messagesElement = document.getElementById('messages');
+  // messagesElement!.innerText = 'Welcome to MultiMath!  Starting new game...'
 
-    const messagesElement = document.getElementById('messages');
-    messagesElement!.innerText = 'Welcome to MultiMath!  Starting new game...'
-    
+  postScore(100, playerName);
+  postScore(-5, playerName)
+};
+
+// passing in a default parameter ( name = 'MultiMath Player') makes the parameter optional
+// void is for when you don't return anything
+const logPlayer = (name: string = "MultiMath Player"): void => {
+  console.log(`New game starting for player: ${name}`);
+};
+
+const getInputValue = (elementId: string): string | undefined => {
+  const inputElement: HTMLInputElement = <HTMLInputElement>(
+    document.getElementById(elementId)
+  );
+
+  if (inputElement.value === "") {
+    return undefined;
+  } else {
+    return inputElement.value;
+  }
+};
+
+const postScore = (
+  score: number,
+  playerName: string = "MultiMath Player"
+): void => {
+  let logger: (value: string) => void;
+  if (score < 0) {
+      logger = logError
+  } else {
+      logger = logMessage
+  }
+
+  const scoreElement: HTMLElement | null = document.getElementById("scores");
+  scoreElement!.innerText = `${score} - ${playerName}`;
+  logger(`Score: ${score}`)
+};
+
+document.getElementById("startGame")!.addEventListener("click", startGame);
+
+const logMessage = (message:string) => console.log(message);
+
+const logError = (err: string): void => {
+    console.error(err)
 }
-
-const logPlayer = (name:string) => {
-    console.log(`New game starting for player: ${name}`)
-}
-
-document.getElementById('startGame')!.addEventListener('click', startGame)
--- js/app.js
+++ js/app.js
@@ -1,8 +1,36 @@
-"use strict";
 const startGame = () => {
-    console.log('check');
-    const messagesElement = document.getElementById('messages');
-    messagesElement.innerText = 'Welcome to MultiMath!  Starting new game...';
+    let playerName = getInputValue("playerName");
+    logPlayer(playerName);
+    postScore(100, playerName);
+    postScore(-5, playerName);
 };
-document.getElementById('startGame').addEventListener('click', startGame);
+const logPlayer = (name = "MultiMath Player") => {
+    console.log(`New game starting for player: ${name}`);
+};
+const getInputValue = (elementId) => {
+    const inputElement = (document.getElementById(elementId));
+    if (inputElement.value === "") {
+        return undefined;
+    }
+    else {
+        return inputElement.value;
+    }
+};
+const postScore = (score, playerName = "MultiMath Player") => {
+    let logger;
+    if (score < 0) {
+        logger = logError;
+    }
+    else {
+        logger = logMessage;
+    }
+    const scoreElement = document.getElementById("scores");
+    scoreElement.innerText = `${score} - ${playerName}`;
+    logger(`Score: ${score}`);
+};
+document.getElementById("startGame").addEventListener("click", startGame);
+const logMessage = (message) => console.log(message);
+const logError = (err) => {
+    console.error(err);
+};
 //# sourceMappingURL=app.js.map
