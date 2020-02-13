d189ac59a9683a415444b82396d6eba17307d9d9
Liz Tiller <elizabeth.tiller@gmail.com>
2020-02-13 16:26:27 -0500

    imports

--- app/app.ts
+++ app/app.ts
@@ -1,6 +1,7 @@
-/// <reference path="player.ts" />
-/// <reference path="game.ts" />
-/// <reference path="utility.ts" />
+import * as Helpers from './utility'
+import { Player } from './player'
+import { Game } from './game';
+
 
 
 let newGame: Game;
@@ -8,10 +9,10 @@ let newGame: Game;
 // add click handler to the start game button
 document.getElementById('startGame')!.addEventListener('click', () => {
   const player: Player = new Player()
-  player.name = Utility.getInputValue('playerName')
+  player.name = Helpers.getValue('playerName');
 
-  const problemCount: number = Number(Utility.getInputValue('problemCount'))
-  const factor: number = Number(Utility.getInputValue('factor'))
+  const problemCount: number = Number(Helpers.getValue('problemCount'))
+  const factor: number = Number(Helpers.getValue('factor'));
 
   newGame = new Game(player, problemCount, factor)
   newGame.displayGame()
--- app/utility.ts
+++ app/utility.ts
@@ -1,6 +1,11 @@
-class Utility {
-    static getInputValue = (elementId: string): string => {
-        const inputElement: HTMLInputElement = <HTMLInputElement>document.getElementById(elementId);
-        return inputElement.value
-    }
+
+const getInputValue = (elementId: string): string => {
+    const inputElement: HTMLInputElement = <HTMLInputElement>document.getElementById(elementId);
+    return inputElement.value
 }
+
+const logger = (message:string):void => {
+    console.log(message)
+}
+
+export {getInputValue as getValue, logger}
